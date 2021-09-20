df7d1f8bd17c9490dd4cd7db5efb9d56f42d087d
Андрей Нестерук <anjeinesty@gmail.com>
2021-09-20 21:28:00 +0300

    feat: add footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burger = document.querySelector('.hamburger');
     const body = document.querySelector('body');
+    const date = document.querySelector('#date');
+
+    date.innerHTML = new Date().getFullYear();
+
     burger.addEventListener('click', function () {
         if (!this.closest('header').classList.contains('open-mobile')) {
             this.closest('header').classList.add('open-mobile');
